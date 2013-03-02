877f35c527be3d7674d0f79526e4c3aaae2c501f
James Azzopardi <james.azzopardi@yahoo.com.au>
2013-03-03 01:09:25 +1100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>title.php</title>
+		<link href="/css/application.css" rel="stylesheet" text="text/css" />
+		<link href="/css/bootstrap.css" rel="stylesheet" text="text/css" />
+		<script src="jquery-191.js"></script>
+		<script src="application.js"></script>	
+	</head>
+	<body>
+		<header>
+
+		</header>
+		<section>
+
+		</section>
+		<footer>
+
+		</footer>
+	</body>
+</html>
