fd3fff7b41c3b5019293c30aac2cc89b9422ced4
James Azzopardi <james.azzopardi@yahoo.com.au>
2013-03-03 18:34:05 +1100

    Fixed Template

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>title.php</title>
-		<link href="/css/application.css" rel="stylesheet" text="text/css" />
-		<link href="/css/bootstrap.css" rel="stylesheet" text="text/css" />
-		<script src="jquery-191.js"></script>
-		<script src="application.js"></script>	
-	</head>
-	<body>
-		<header>
+		<title>Yep</title>
 
-		</header>
-		<section>
+        <link href="css/bootstrap.css" rel="stylesheet">
+    	<link rel="stylesheet" href="css/application.css" media="Screen" type="text/css" />
 
-		</section>
-		<footer>
+   		<script type="text/javascript" src="/js/jquery-1.9.1.js"></script>
+   		<script type="text/javascript" src="/js/application.js"></script>
+   	</head>
 
-		</footer>
-	</body>
+   	<body>
+   		<header>
+   			This is a Template Application
+   		</header>
+
+   		<section>
+   			<h3>Implemented</h3>
+   			<ul>
+   				<li>JQuery</li>
+   				<li>Backbone</li>
+   				<li>Empty Stylesheet</li>
+   				<li>Empty Java Sheet</li>
+   			</ul>
+   		</section>
+   		<footer>
+   			A JParty Production
+   		</footer>
+   	</body>
 </html>
